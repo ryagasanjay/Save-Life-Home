4a4fa63f19c478ffc03044ca095f66f798b227f1
sanjay <ryagasanjay@gmail.com>
2020-06-16 21:02:14 -0400

    Contact Tracing changes. With this all database write forms are complete

--- app/src/main/java/com/example/savelifehome/NewsOrAnnouncements.java
+++ app/src/main/java/com/example/savelifehome/NewsOrAnnouncements.java
@@ -19,8 +19,50 @@ public class NewsOrAnnouncements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_or_announcements);
+
         mListView = findViewById(R.id.newslist);
         aAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, news);
         mListView.setAdapter(aAdapter);
+
+        /*
+        final TextView  jsonValue = findViewById(R.id.textView29);
+        Button btn = findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask asyncTask = new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        OkHttpClient client = new OkHttpClient().newBuilder()
+                                .build();
+                        Request request = new Request.Builder()
+                                .url("https://newsapi.org/v2/everything?q=COVID&from=2020-05-16&sortBy=publishedAt&apiKey=eef9095b8dfa4f76bcd8e2990dbaecf9&pageSize=2&page=1")
+                                .method("GET", null)
+                                .addHeader("Cookie", "__cfduid=d8f978037fc501a3b0c984ba4b187bbd01592347123")
+                                .build();
+                        try {
+                            Response response = client.newCall(request).execute();
+                            Log.v("sanjay", String.valueOf(response.body()));
+                            return response.body().toString();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+
+                    protected void onPostExecute(Object o){
+
+                            jsonValue.setText(o.toString());
+                    }
+                };
+
+            }
+        }); */
+
+
+//
     }
+
 }
