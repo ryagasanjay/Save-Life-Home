6f8c14125a83f7777a2048d5c797242787f2ef88
sanjay <ryagasanjay@gmail.com>
2020-06-16 21:53:51 -0400

    Contact Tracing changes. With this all database write forms are complete

--- app/src/main/java/com/example/savelifehome/NewsOrAnnouncements.java
+++ app/src/main/java/com/example/savelifehome/NewsOrAnnouncements.java
@@ -64,5 +64,4 @@ public class NewsOrAnnouncements extends AppCompatActivity {
 
 //
     }
-
 }
--- app/src/main/java/com/example/savelifehome/Profile.java
+++ app/src/main/java/com/example/savelifehome/Profile.java
@@ -1,11 +1,14 @@
 package com.example.savelifehome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,12 +55,25 @@ public class Profile extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view.findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), Signup.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);//makesure user cant go back
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
